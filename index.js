1abaeabf0c19f9a941ce86b9ba438ba1f6d524ab
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-02-02 01:36:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for(let i = 0; i < cart.lenght; i++) {
+  for(let i = 0; i < cart.length; i++) {
     if(cart[i].hasOwnProperty(item)) {
-      cart.remove(i);
+      cart.splice(i, 1);
       return cart;
     }
   }
